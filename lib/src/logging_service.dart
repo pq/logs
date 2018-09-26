cb4c03639fd48ef5443e19c8d9b61be08a15f92f
pq <pquitslund@gmail.com>
2018-09-26 14:39:51 -0700

    more registration tweaks

--- lib/src/logging_service.dart
+++ lib/src/logging_service.dart
@@ -64,7 +64,7 @@ class LoggingService {
   /// flutter foundation binding (see e.g., https://github.com/flutter/flutter/pull/21505).
   void initServiceExtensions() {
     _registerServiceExtension(
-        name: 'logs.enable',
+        name: 'enable',
         callback: (Map<String, Object> parameters) async {
           final String channel = parameters['channel'];
           if (channel != null) {
@@ -75,7 +75,7 @@ class LoggingService {
           return <String, dynamic>{};
         });
     _registerServiceExtension(
-        name: 'logs.loggingChannels',
+        name: 'loggingChannels',
         callback: (Map<String, dynamic> parameters) async => _channels
             .map((channel, description) => MapEntry(channel, <String, String>{
                   'enabled': shouldLog(channel).toString(),
@@ -111,7 +111,7 @@ class LoggingService {
       {@required String name, @required _ServiceExtensionCallback callback}) {
     assert(name != null);
     assert(callback != null);
-    final String methodName = 'ext.flutter.$name';
+    final String methodName = 'ext.flutter.logs.$name';
     developer.registerExtension(name,
         (String method, Map<String, String> parameters) async {
       assert(method == methodName);
