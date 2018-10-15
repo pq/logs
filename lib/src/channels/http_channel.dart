2de3fadac0121ec09c31d43bb4d3485601d3dcf4
pq <pquitslund@gmail.com>
2018-10-15 06:06:24 -0700

    review feedback

--- lib/src/channels/http_channel.dart
+++ lib/src/channels/http_channel.dart
@@ -1,5 +1,4 @@
 /// Simple WIP http channel based on POC in https://github.com/pq/logs/issues/6
-
 import 'dart:async';
 import 'dart:io';
 
@@ -18,22 +17,17 @@ void installHttpChannel() {
   HttpOverrides.global = _httpOverrides;
 }
 
+void _todo(String msg) {
+  print('todo: $msg');
+}
+
 class LoggingHttpClient implements HttpClient {
   HttpClient proxy;
 
   @override
-  Duration connectionTimeout;
-
-  @override
-  Duration idleTimeout;
-
-  @override
-  int maxConnectionsPerHost;
-
-  @override
   String userAgent;
 
-  int count = 1;
+  int _count = 1;
 
   LoggingHttpClient({SecurityContext context}) {
     HttpOverrides.global = null;
@@ -44,15 +38,15 @@ class LoggingHttpClient implements HttpClient {
   @override
   set authenticate(
       Future<bool> Function(Uri url, String scheme, String realm) f) {
-    todo('authenticate');
+    _todo('authenticate');
     proxy.authenticate = f;
   }
 
   @override
   set authenticateProxy(
-      Future<bool> Function(String host, int port, String scheme, String realm)
-          f) {
-    todo('authenticateProxy');
+    Future<bool> Function(String host, int port, String scheme, String realm) f,
+  ) {
+    _todo('authenticateProxy');
     proxy.authenticateProxy = f;
   }
 
@@ -66,94 +60,119 @@ class LoggingHttpClient implements HttpClient {
   @override
   set badCertificateCallback(
       bool Function(X509Certificate cert, String host, int port) callback) {
-    todo('badCertificateCallback');
+    _todo('badCertificateCallback');
     proxy.badCertificateCallback = callback;
   }
 
   @override
+  Duration get connectionTimeout => proxy.connectionTimeout;
+
+  @override
+  set connectionTimeout(Duration connectionTimeout) {
+    proxy.connectionTimeout = connectionTimeout;
+  }
+
+  @override
   set findProxy(String Function(Uri url) f) {
-    todo('findProxy');
+    _todo('findProxy');
     proxy.findProxy = f;
+  }
+
+  @override
+  Duration get idleTimeout => proxy.idleTimeout;
+
+  @override
+  set idleTimeout(Duration idleTimeout) {
+    proxy.idleTimeout = idleTimeout;
+  }
+
+  @override
+  int get maxConnectionsPerHost => proxy.maxConnectionsPerHost;
+
+  @override
+  set maxConnectionsPerHost(int maxConnectionsPerHost) {
+    proxy.maxConnectionsPerHost = maxConnectionsPerHost;
   }
 
   @override
   void addCredentials(
       Uri url, String realm, HttpClientCredentials credentials) {
-    todo('addCredentials');
+    _todo('addCredentials');
     proxy.addCredentials(url, realm, credentials);
   }
 
   @override
   void addProxyCredentials(
       String host, int port, String realm, HttpClientCredentials credentials) {
-    todo('addProxyCredentials');
+    _todo('addProxyCredentials');
     proxy.addProxyCredentials(host, port, realm, credentials);
   }
 
   @override
   void close({bool force = false}) {
-    todo('close');
+    _todo('close');
     proxy.close(force: force);
   }
 
   @override
   Future<HttpClientRequest> delete(String host, int port, String path) {
-    todo('delete');
+    _todo('delete');
     return proxy.delete(host, port, path);
   }
 
   @override
   Future<HttpClientRequest> deleteUrl(Uri url) {
-    todo('deleteUrl');
+    _todo('deleteUrl');
     return proxy.deleteUrl(url);
   }
 
   @override
   Future<HttpClientRequest> get(String host, int port, String path) {
-    _log.log(() => 'getUrl: $host $port $path');
+    // todo (pq): consider same logic as open (but w/ GET added)
+    _todo('getUrl');
     return proxy.get(host, port, path);
   }
 
   @override
   Future<HttpClientRequest> getUrl(Uri url) {
-    _log.log(() => 'getUrl: $url');
+    // todo (pq): consider same logic as open (but w/ GET added)
+    _todo('getUrl');
     return proxy.getUrl(url);
   }
 
   @override
   Future<HttpClientRequest> head(String host, int port, String path) {
-    todo('head');
+    _todo('head');
     return proxy.head(host, port, path);
   }
 
   @override
   Future<HttpClientRequest> headUrl(Uri url) {
-    todo('headUrl');
+    _todo('headUrl');
     return proxy.headUrl(url);
   }
 
   @override
   Future<HttpClientRequest> open(
       String method, String host, int port, String path) {
-    todo('open');
+    _todo('open');
     return proxy.open(method, host, port, path);
   }
 
   @override
   Future<HttpClientRequest> openUrl(String method, Uri url) {
-    final int id = count++;
+    final int id = _count++;
 
     // #1 • GET • https://flutter.io open
-    _log.log(() => '#$id \u2022 $method \u2022 $url open');
+    _log.log(() => '#$id • $method • $url open');
 
     Future<HttpClientRequest> request = proxy.openUrl(method, url);
     return request.then((HttpClientRequest req) {
-      // todo (pq): consider putting id in sequence number?
-      _log.log(() => '#$id \u2022 $method \u2022 $url request ready');
+      _log.log(() => '#$id • $method • $url request ready');
 
       req.done.then((HttpClientResponse response) {
         _log.log(() =>
-            '#$id \u2022 $method \u2022 $url ${response.statusCode} ${response.reasonPhrase} ${response.contentLength} bytes');
+            '#$id • $method • $url ${response.statusCode} ${response.reasonPhrase} ${response.contentLength} bytes');
       });
 
       return req;
@@ -162,43 +181,39 @@ class LoggingHttpClient implements HttpClient {
 
   @override
   Future<HttpClientRequest> patch(String host, int port, String path) {
-    todo('patch');
+    _todo('patch');
     return proxy.patch(host, port, path);
   }
 
   @override
   Future<HttpClientRequest> patchUrl(Uri url) {
-    todo('patchUrl');
+    _todo('patchUrl');
     return proxy.patchUrl(url);
   }
 
   @override
   Future<HttpClientRequest> post(String host, int port, String path) {
-    todo('post');
+    _todo('post');
     return proxy.post(host, port, path);
   }
 
   @override
   Future<HttpClientRequest> postUrl(Uri url) {
-    todo('postUrl');
+    _todo('postUrl');
     return proxy.postUrl(url);
   }
 
   @override
   Future<HttpClientRequest> put(String host, int port, String path) {
-    todo('put');
+    _todo('put');
     return proxy.put(host, port, path);
   }
 
   @override
   Future<HttpClientRequest> putUrl(Uri url) {
-    todo('putUrl');
+    _todo('putUrl');
     return proxy.putUrl(url);
   }
-}
-
-void todo(String msg) {
-  print('TODO: $msg');
 }
 
 class _HttpOverrides extends HttpOverrides {
